b89f28c5dd46cedd6bba2eb70e86fdedc0e66f67
David <davidaase@hotmail.com>
2020-02-10 18:20:10 +0100

    Add empty constructor to java example

--- src/main/java/io/javalin/example/java/user/NewUserRequest.java
+++ src/main/java/io/javalin/example/java/user/NewUserRequest.java
@@ -4,6 +4,9 @@ public class NewUserRequest {
     public String name;
     public String email;
 
+    public NewUserRequest() {
+    }
+
     public NewUserRequest(String name, String email) {
         this.name = name;
         this.email = email;
